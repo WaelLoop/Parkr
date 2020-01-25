00e4e10a4120791d11d5cc992f14ef04ade79594
affanjilani <obiaffan@gmail.com>
2020-01-25 14:26:43 -0500

    Initialized attributes for parking_spots, finished table creation and drop functions

--- server/db/initialize_db.py
+++ server/db/initialize_db.py
@@ -1,25 +1,53 @@
 # Initalizes the database tables and seeds
 from credentials import db_name, user, pw, db_url
+from sql.tables import tables
 import psycopg2 as pg
 
-def initTableIfDoesntExist(conn, table_name):
-
-    # Create cursor to db
-    cur = conn.cursor()
-
+def initTableIfDoesntExist(conn, table_name, attributes):
     # Create
     command = f"""
     CREATE TABLE IF NOT EXISTS {table_name}(
-    ID SERIAL PRIMARY KEY,
-    ADDRESS VARCHAR(255),
-    OCCUPIED BOOLEAN
-    )
     """
+    command += attributes
+    
+    command += """)"""
 
     print(command)
+    try:
+        # Create cursor to db
+        cur = conn.cursor()
+
+        cur.execute(command)
+
+        cur.close()
+
+        conn.commit()
+        
+    except:
+        print(f"Error added table{table_name}")
+
+def dropTableIfExists(conn, table_name):
+    command = f"""
+    DROP TABLE IF EXISTS {table_name} CASCADE
+    """
+
+    try:
+        cur = conn.cursor()
+
+        cur.execute(command)
+
+        cur.close()
+
+        conn.commit()
+    except:
+        print(f"Error deleting table {table_name}")
 
 
 if __name__ == "__main__":
     conn = pg.connect(host=db_url, database=db_name, user=user, password=pw)
-    initTableIfDoesntExist(conn, 'parking_spots')
+
+    initTableIfDoesntExist(conn, 'parking_spots', tables['parking_spots'])
+    #dropTableIfExists(conn, 'parking_spots')
+
+    conn.close()
     pass
---
+++ server/db/sql/tables.py
@@ -0,0 +1,9 @@
+parking_spot_attrs = """
+    ID SERIAL PRIMARY KEY,
+    LICENCE CHAR(6),
+    ADDRESS VARCHAR(255)
+""" 
+
+tables = {
+    "parking_spots": parking_spot_attrs
+}
