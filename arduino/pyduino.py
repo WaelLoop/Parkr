0d2446b347f8f44da407b47647df99a21c5dd2ef
waelloop <alhashemiw@gmail.com>
2020-01-26 01:06:35 -0500

    added mac configuration

--- arduino/pyduino.py
+++ arduino/pyduino.py
@@ -1,19 +1,27 @@
 import serial
 
 
-arduinoData = serial.Serial('com3', 9600)
 
-def led_on():
-    arduinoData.write('1'.encode())
+def trigger(input):
 
-def led_off():
-    arduinoData.write('0'.encode())
+    arduinoData = serial.Serial('/dev/cu.usbmodem14401', 9600)
 
-def trigger(number):
-    if number == 1:
-        led_on()
-    else:
-        led_off()
+    # led1 off
+    if input == 0:
+        arduinoData.write('0'.encode())
+    # led1 on
+    elif input == 1:
+        arduinoData.write('1'.encode())
+    # led2 off
+    elif input == 2:
+        arduinoData.write('2'.encode())
+    # led2 on
+    elif input == 3:
+        arduinoData.write('3'.encode())
+    # led3 off
+    elif input == 4:
+        arduinoData.write('4'.encode())
+    # led3 on
+    elif input == 5:
+        arduinoData.write('5'.encode())
 
-if __name__ == '__main__':
-    trigger(1)
